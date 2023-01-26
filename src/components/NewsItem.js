f4350ff55688cf7c2585e2219d8ad7f9b67757b1
Aryan Gupta <aryang23936@gmail.com>
2023-01-27 00:21:15 +0530

    fifth commit(Adding categories and protoTypyes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: '18rem'}}>
+        <div className="card" >
             <img src={!imageUrl?"https://image.cnbcfm.com/api/v1/image/107090863-1658245353699-NUP_198367_01036_copy.jpg?v=1674483596&w=1920&h=1080":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
